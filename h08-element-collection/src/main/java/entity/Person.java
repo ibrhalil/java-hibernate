83651f4da887020f739312f92edb6a00c1178289
Halil AYDIN <ibrhalilaydn@gmail.com>
2022-09-04 21:16:16 +0300

    collection degiskenlerinin ilk degerleri bos bir nesne olarak tanÄimlanmali

--- h08-element-collection/src/main/java/entity/Person.java
+++ h08-element-collection/src/main/java/entity/Person.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -26,10 +28,10 @@ public class Person implements Serializable {
     @ElementCollection
     @CollectionTable(name = "T_PERSON_PHONE_NUM", joinColumns = @JoinColumn(name = "PERSON_ID"))
     @Column(name = "P_NUMBER", length = 11)
-    private List<String> phoneNumbers;
+    private List<String> phoneNumbers = new ArrayList<>();
 
     @ElementCollection
     @CollectionTable(name = "T_PERSON_EMAIL", joinColumns = @JoinColumn(name = "PERSON_ID"))
     @Column(name = "EMAIL", unique = true)
-    private Set<String> emails;
+    private Set<String> emails = new HashSet<>();
 }
